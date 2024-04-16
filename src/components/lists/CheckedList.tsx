ae5cb8fc79f8c194af3bcb9cf90f84d83f31e271
kevin-martins <kev.martinsvieira@gmail.com>
2024-04-16 12:05:01 +0200

    feat: add checked list component

---
+++ src/components/lists/CheckedList.tsx
@@ -0,0 +1,52 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+
+const infoVariants = {
+  hidden: { y: -30, opacity: 0 },
+  show: { y: 0, opacity: 1 }
+};
+
+type ElementProps = {
+  text: string
+}
+
+const Elements = ({ text }: ElementProps) => {
+  return (
+    <motion.li
+      variants={infoVariants}
+      className="flex flex-row gap-[.5px]"
+    >
+      <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="shrink-0 my-auto mr-1">
+        <path d="M6.35588 11.8345L1.61455 7.17002L0 8.7472L6.35588 15L20 1.57718L18.3968 0L6.35588 11.8345Z" fill="green" />
+      </svg>
+      <p>{text}</p>
+    </motion.li>
+  );
+};
+
+const wrapperVariants = {
+  show: {
+    transition: { staggerChildren: 0.25 }
+  }
+};
+
+type CheckedList = {
+  list: string[]
+}
+
+const CheckedList = ({ list }: CheckedList) => {
+  return (
+    <motion.ul
+      variants={wrapperVariants}
+      initial='hidden'
+      animate='show'
+      className='flex flex-col z-10 text-black dark:text-white'
+    >
+      {list.map((text, i) => (
+        <Elements key={i} text={text} />
+      ))}
+    </motion.ul>
+  );
+};
+
+export default CheckedList;

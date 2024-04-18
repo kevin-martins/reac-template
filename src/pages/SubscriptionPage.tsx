74ec30ed05da805f6755fa081b2834aa53bb8765
Kevin Martins <kev.martinsvieira@gmail.com>
2024-04-19 00:18:41 +0200

    refacto: use wrapper component as parent for pricing template

--- src/pages/SubscriptionPage.tsx
+++ src/pages/SubscriptionPage.tsx
@@ -1,18 +1,5 @@
 import Pricing from '../components/Pricing';
-
-type Props = {
-  title: string
-  children: JSX.Element
-};
-
-const Wrapper = ({ title, children }: Props) => (
-  <div className="py-2">
-    <h2 className="text-xl">{title}</h2>
-    <div className="bg-white min-h-[200px] flex items-center justify-center border border-slate-300">
-      {children}
-    </div>
-  </div>
-);
+import Wrapper from '../components/Wrapper';
 
 const SubscriptionPage = () => {
   return (

1a0206c13fc65bc72970c6e9720a0c1b69bca61f
Kevin Martins <kev.martinsvieira@gmail.com>
2024-04-20 01:36:50 +0200

    fix: add padding on Wrapper

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -6,7 +6,7 @@ type Props = {
 const Wrapper = ({ title, children }: Props) => (
   <div className="py-2">
     <h2 className="text-xl">{title}</h2>
-    <div className="bg-white min-h-[250px] flex items-center justify-center border border-black">
+    <div className="bg-white min-h-[250px] p-10 flex items-center justify-center border border-black">
       {children}
     </div>
   </div>

ae7227a0067d512afd7c3adc28122511b6c4556d
Kevin Martins <kev.martinsvieira@gmail.com>
2024-04-20 12:43:34 +0200

    feat: add padding props in the wrapper comp

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,12 +1,13 @@
 type Props = {
   title: string
   children: JSX.Element
+  padding?: boolean
 };
 
-const Wrapper = ({ title, children }: Props) => (
+const Wrapper = ({ title, children, padding = true }: Props) => (
   <div className="py-2">
     <h2 className="text-xl">{title}</h2>
-    <div className="bg-white min-h-[250px] p-10 flex items-center justify-center border border-black">
+    <div className={`bg-white min-h-[250px] flex items-center justify-center border border-black ${padding ? 'py-10' : ''}`}>
       {children}
     </div>
   </div>

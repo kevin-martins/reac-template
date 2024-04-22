283d352becb3e0ea77657791c6aa55b91a5a73a9
Kevin Martins <kev.martinsvieira@gmail.com>
2024-04-22 22:49:44 +0200

    style: change close button close

--- src/components/lists/ContactList.tsx
+++ src/components/lists/ContactList.tsx
@@ -161,7 +161,7 @@ const ContactList = () => {
           >
             <button
               onClick={() => { setOpen(false) }}
-              className="absolute right-2 top-2 text-right hover:text-red-600 scale-[200%] hover:rotate-[90deg] text-black duration-300"
+              className="absolute right-2 top-2 text-right hover:text-red-600 scale-[200%] hover:rotate-[90deg] text-white duration-300"
             >
               <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                 <path fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M368 368 144 144m224 0L144 368" />

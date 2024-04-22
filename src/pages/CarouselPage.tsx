b4c42f26ebffc2ca95b9d80ab343fd908d835df0
Kevin Martins <kev.martinsvieira@gmail.com>
2024-04-22 02:48:35 +0200

    chore: remove comments

--- src/pages/CarouselPage.tsx
+++ src/pages/CarouselPage.tsx
@@ -7,11 +7,6 @@ const CarouselPage = () => {
       <Wrapper title='Swipe carousel' padding={false}>
         <SwipeCarousel />
       </Wrapper>
-      {/* <div className="py-2">
-        <div className="bg-white min-h-[250px] grid place-content-center border border-black">
-        <SwipeCarousel />
-        </div>
-      </div> */}
     </div>
   );
 };

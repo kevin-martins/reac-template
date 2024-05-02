43adb8f7ac579b99c9b198093b2d4c6bd6624bc0
kevin-martins <kev.martinsvieira@gmail.com>
2024-05-02 14:12:49 +0200

    feat: create effect on navbar elements

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,24 +1,67 @@
 import { Outlet } from "react-router-dom";
 import { motion } from 'framer-motion';
 import { LinkProps } from "../models/route";
+import { useEffect, useState } from "react";
+
+const staggerVariants = {
+  visible: {
+    transition: {
+      staggerChildren: 0.05
+    }
+  }
+};
+
+const letterVariants = {
+  hidden: { y: -20 },
+  visible: { y: 0 }
+};
 
 const links: LinkProps[] = [
   {
     to: '#',
-    text: 'Subscriptions'
+    text: 'Home'
+  },
+  {
+    to: '#/components',
+    text: 'Components'
   }
 ];
 
+const NavigationLink = (link: LinkProps) => {
+  const [isHover, setIsHover] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsHover(false);
+    }, 100)
+  }, [isHover])
+  return (
+    <motion.a
+      key={link.text}
+      href={link.to}
+      variants={staggerVariants}
+      onMouseEnter={() => setIsHover(true)}
+      animate={isHover ? "hidden" : "visible"}
+      className='inline-flex ml-5'
+    >
+      {link.text.split('').map((letter, i) => (
+        <motion.span
+          key={i}
+          variants={letterVariants}
+          className="font-[FontAwesome] pointer-events-none"
+        >
+          {letter}
+        </motion.span>
+      ))}
+    </motion.a>
+  )
+}
+
 const NavBar = () => {
   return (
-    <div className='fixed h-[54px] z-50 w-full bg-slate-700 shadow-lg text-white'>
+    <div className='fixed h-[54px] z-50 w-full bg-slate-700 shadow-lg text-white flex items-center'>
       {links.map((link, i) => (
-        <motion.a
-          key={i}
-          href={link.to}
-        >
-          {link.text}
-        </motion.a>
+        <NavigationLink key={i} {...link} />
       ))}
     </div>
   );
@@ -28,7 +71,9 @@ const Root = () => {
   return (
     <main className="h-full">
       <NavBar />
-      <Outlet />
+      <div className="relative px-3 col-span-1">
+        <Outlet />
+      </div>
     </main>
   );
 };

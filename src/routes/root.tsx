4a5f5fa149b187adda83623f5f860b8a22f2b950
kevin-martins <kev.martinsvieira@gmail.com>
2024-05-02 12:05:53 +0200

    feat: move navbar to root

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,5 +1,28 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "../components/navbar/NavBar";
+import { motion } from 'framer-motion';
+import { LinkProps } from "../models/route";
+
+const links: LinkProps[] = [
+  {
+    to: '#',
+    text: 'Subscriptions'
+  }
+];
+
+const NavBar = () => {
+  return (
+    <div className='fixed h-[54px] z-50 w-full bg-slate-700 shadow-lg text-white'>
+      {links.map((link, i) => (
+        <motion.a
+          key={i}
+          href={link.to}
+        >
+          {link.text}
+        </motion.a>
+      ))}
+    </div>
+  );
+};
 
 const Root = () => {
   return (

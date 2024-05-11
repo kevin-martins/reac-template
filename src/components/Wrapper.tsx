2a207954a26d59bcec1a2ce325e3ec70c6d7c86d
Kevin Martins <kev.martinsvieira@gmail.com>
2024-05-11 15:57:03 +0200

    fix: add spacing to wrapper

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -5,8 +5,8 @@ type Props = {
 };
 
 const Wrapper = ({ title, children, padding = true }: Props) => (
-  <div className="py-2">
-    <h2 className="text-xl">{title}</h2>
+  <div className="py-4">
+    <h2 className="text-xl mb-1">{title}</h2>
     <div className={`bg-white min-h-[250px] flex items-center justify-center border border-black ${padding ? 'py-10' : ''}`}>
       {children}
     </div>
